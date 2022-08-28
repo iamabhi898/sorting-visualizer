307223b2116f242a9da0273948c2a7c72901fcb3
Abhishek Tripathi <abhishek830318@gmail.com>
2022-08-28 18:41:12 +0530

    first base - slider, button created

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,50 +2,70 @@ import React, { useState, useEffect } from "react";
 import "./SortingVisualizer.css";
 
 function SortingVisualizer() {
+  // USE STATE HOOKS, VALUES :::
   // array
   const [arr, setArr] = useState([]);
   // dummy count
-  const [count, setCount] = useState(0);
+  const [handleGenerateRandomArray, SetHandleGenerateRandomArray] =
+    useState(false);
+  // number of bars
+  const [numOfBars, setNumOfBars] = useState(100);
 
+  // GENERATE NEW RANDOM ARRAY LOGIC :::
   // similar to componentDidMount & componentDidUpdate
   useEffect(() => {
-    document.title = `clicked ${count} times`;
-  });
+    generateRandomArr(numOfBars);
+  }, [handleGenerateRandomArray, numOfBars]);
 
-  function incrementCount() {
-    setCount(count + 1);
+  function toggleSetHandlerGenerateRandomArray() {
+    SetHandleGenerateRandomArray(!handleGenerateRandomArray);
   }
 
-  function generateRandomArr() {
+  function generateRandomArr(n) {
     const temp = [];
-    for (let i = 0; i < 10; i++) {
-      temp.push(randomNumberInRange(5, 1000));
+    for (let i = 0; i < n; i++) {
+      temp.push(randomNumberInRange(5, 95));
     }
     setArr(temp);
   }
 
   return (
     <div className="svWrapper">
-      <h1>Sorting Visualizer - clicked {count}</h1>
-      <button
-        onClick={() => {
-          incrementCount();
-          generateRandomArr();
-        }}
-      >
-        Click Me
-      </button>
-      {arr.map((num, key) => {
-        return (
-          <p className={key}>
-            {num} | {key}
-          </p>
-        );
-      })}
+      <h1>Sorting Visualizer</h1>
+      <div className="inputsWrapper">
+        <button onClick={toggleSetHandlerGenerateRandomArray}>
+          Generate New Random Array
+        </button>
+        <input
+          type="range"
+          id="slider"
+          min={"5"}
+          max={"200"}
+          value={numOfBars}
+          onChange={(event) => {
+            setNumOfBars(event.target.value);
+          }}
+        />
+      </div>
+      <div className="barsWrapper">
+        {arr.map((num, key) => {
+          let wid = (100 * 1.0) / ((2 * arr.length + 1) * 1.0);
+          return (
+            <div
+              className="bar"
+              key={key}
+              style={{ height: num + "%", width: wid + "%" }}
+            >
+              {/* {num} */}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
+// GET RANDOM NUMBER
 function randomNumberInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
