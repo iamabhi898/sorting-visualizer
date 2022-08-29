96529122215d33f60b00611da6b12a49e1e2fada
Abhishek Tripathi <abhishek830318@gmail.com>
2022-08-29 20:19:47 +0530

    toggle bar view - added & UI improved

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -10,6 +10,8 @@ function SortingVisualizer() {
     useState(false);
   // number of bars
   const [numOfBars, setNumOfBars] = useState(100);
+  // flex-start||end - toggle bars position
+  const [bottomUp, setBottomUp] = useState(true);
 
   // GENERATE NEW RANDOM ARRAY LOGIC :::
   // similar to componentDidMount & componentDidUpdate
@@ -24,18 +26,33 @@ function SortingVisualizer() {
   function generateRandomArr(n) {
     const temp = [];
     for (let i = 0; i < n; i++) {
-      temp.push(randomNumberInRange(5, 95));
+      temp.push(randomNumberInRange(5, 100));
     }
     setArr(temp);
   }
 
+  // TOGGLE BAR VIEW - BOTTOM-UP or TOP-DOWN :::
+  function toggleBarView() {
+    setBottomUp(!bottomUp);
+  }
+
   return (
     <div className="svWrapper">
-      <h1>Sorting Visualizer</h1>
       <div className="inputsWrapper">
-        <button onClick={toggleSetHandlerGenerateRandomArray}>
-          Generate New Random Array
-        </button>
+        <div className="upperButtonWrapper">
+          {/* BUTTON ::: GENERATE NEW RANDOM ARRAY */}
+          <button
+            className="button uppperBtn"
+            onClick={toggleSetHandlerGenerateRandomArray}
+          >
+            Generate New Random Array
+          </button>
+          {/* BUTTON ::: TOGGLE BAR VIEW */}
+          <button className="button uppperBtn" onClick={toggleBarView}>
+            Toggle Bar View
+          </button>
+          {/* SLIDER ::: NUMBER OF BARS */}
+        </div>
         <input
           type="range"
           id="slider"
@@ -47,7 +64,12 @@ function SortingVisualizer() {
           }}
         />
       </div>
-      <div className="barsWrapper">
+      {/* CONTAINER ::: BARS */}
+      <div
+        className="barsWrapper"
+        style={{ alignItems: "flex-" + (bottomUp ? "start" : "end") }}
+      >
+        {/* BARS  */}
         {arr.map((num, key) => {
           let wid = (100 * 1.0) / ((2 * arr.length + 1) * 1.0);
           return (
@@ -56,7 +78,7 @@ function SortingVisualizer() {
               key={key}
               style={{ height: num + "%", width: wid + "%" }}
             >
-              {/* {num} */}
+              {numOfBars <= 15 ? num : null}
             </div>
           );
         })}
